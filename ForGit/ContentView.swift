2bc38d279540454b6bf478caff474812ec744ade
beksultanmusaev <musaeviosdev@gmail.com>
2024-01-08 18:39:17 +0600

    FirstCommit

--- ForGit/ContentView.swift
+++ ForGit/ContentView.swift
@@ -14,6 +14,13 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+            
+            Text("HHHHHHHHHH")
+            
+            Text("HHHHHHHHHH")
+            
+            Text("HHHHHHHHHH")
+
         }
         .padding()
     }
