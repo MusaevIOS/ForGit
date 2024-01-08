ad7044d164fbd1fa4af96047a3eba4edaeef6f1e
beksultanmusaev <musaeviosdev@gmail.com>
2024-01-08 18:37:32 +0600

    Initial Commit

---
+++ ForGit/ForGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForGitApp.swift
+//  ForGit
+//
+//  Created by Бексултан Мусаев on 8/1/24.
+//
+
+import SwiftUI
+
+@main
+struct ForGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
